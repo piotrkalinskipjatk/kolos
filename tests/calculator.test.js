df850fe3eac3cd6e4a4e42cc32cef9b9a4007ae2
s26994 <s26994@pjwstk.edu.pl>
2025-06-13 12:43:32 +0200

    commit

---
+++ tests/calculator.test.js
@@ -0,0 +1,21 @@
+const { add, subtract, multiply, divide } = require('../src/calculator');
+
+test('addition', () => {
+  expect(add(3, 2)).toBe(5);
+});
+
+test('subtraction', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+test('multiplication', () => {
+  expect(multiply(4, 2)).toBe(8);
+});
+
+test('division', () => {
+  expect(divide(10, 2)).toBe(5);
+});
+
+test('division by zero throws', () => {
+  expect(() => divide(5, 0)).toThrow("Division by zero");
+});
